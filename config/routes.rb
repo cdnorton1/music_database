4fb52c411a7c6fae3aa9d106943bb4c22f0db43d
Chris Norton <cnorton1@gmail.com>
2016-02-10 16:58:29 -0500

    styled artist index

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 root 'hip#hop'
 
   get '/about' => 'hip#about'
-
+  get '/artists' => 'artists#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
